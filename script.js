73483b64603cd0ad1bdb64e665c6033febadb3da
anthonytobar <37843535+anthonytobar@users.noreply.github.com>
2019-02-26 13:33:02 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function nextImage(el){
+	if (el.src.match("images/lemon_slot.PNG"){
+} else if (el.src.match("images/lemon_slot.PNG"){
+		el.src = "images/orange_slot.PNG";
+	} else if (el.src = "images/orange_slot.PNG"){
+		el.src = "images/cherry_slot.PNG";
+	} else {
+		// do nothing
+	}
+}
+
+
+function playSound() {
+ document.getElementById('you-win').play();
+}
